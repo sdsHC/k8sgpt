b878801f3e0089e212e464948ef2403adfe380a3
k-choon <kichoon0512.lee@samsung.com>
2023-11-27 09:17:17 +0900

    prompt and logs

--- pkg/ai/prompts.go
+++ pkg/ai/prompts.go
@@ -1,11 +1,18 @@
 package ai
 
 const (
+	//default_prompt = `Simplify the following Kubernetes error message delimited by triple dashes written in --- %s --- language; --- %s ---.
+	//Provide the most possible solution in a step by step style in no more than 280 characters. Write the output in the following format:
+	//Error: {Explain error here}
+	//Solution: {Step by step solution here}
+	//`
 	default_prompt = `Simplify the following Kubernetes error message delimited by triple dashes written in --- %s --- language; --- %s ---.
-	Provide the most possible solution in a step by step style in no more than 280 characters. Write the output in the following format:
-	Error: {Explain error here}
-	Solution: {Step by step solution here}
-	`
+        Provide the most possible solution in a step by step style in no more than 2000 characters. Write the output in the following format:
+        Error: {Explain error here}
+        Solution: {Step by step solution here}
+        Object: {Related Kubernetes object}
+        `
+
 	trivy_vuln_prompt = "Explain the following trivy scan result and the detail risk or root cause of the CVE ID, then provide a solution. Response in %s: %s"
 	trivy_conf_prompt = "Explain the following trivy scan result and the detail risk or root cause of the security check, then provide a solution."
 )
